0787bcb31ee423501e43b22617bbb815e015a2e5
yupolishchuk <yumuscletank@gmail.com>
2019-09-16 17:42:12 +0300

    Add DI container

---
+++ bootstrap/ContainerBootstrap.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\bootstrap;
+
+use app\dispatchers\EventDispatcher;
+use app\dispatchers\DummyEventDispatcher;
+use app\repositories\Hydrator;
+use app\repositories\SqlEmployeeRepository;
+use app\repositories\EmployeeRepository;
+use ProxyManager\Factory\LazyLoadingValueHolderFactory;
+use yii\base\BootstrapInterface;
+use yii\di\Instance;
+
+class ContainerBootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        $container = \Yii::$container;
+
+        $container->setSingleton(EventDispatcher::class, DummyEventDispatcher::class);
+
+        $container->setSingleton(Hydrator::class);
+
+        $container->setSingleton(LazyLoadingValueHolderFactory::class);
+
+        $container->setSingleton('db', function () use ($app) {
+            return $app->db;
+        });
+
+        $container->setSingleton(EmployeeRepository::class, SqlEmployeeRepository::class, [
+            Instance::of('db'),
+        ]);
+    }
+}
